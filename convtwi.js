0c3003ffd39574a3ebee78f64534dfe65cca1d84
blatank <paddocknote@gmail.com>
2023-08-20 15:19:50 +0900

    一通りの機能を完成

--- convtwi.js
+++ convtwi.js
@@ -5,9 +5,27 @@
   const btn = document.getElementById('conv');
 
   btn.onclick = (event) => {
-    let url = urlEdit.innerText;
-    alert(url);
-    url.replace(/^https:\/\/x.com/, "https://twitter.com");
-    alert(url);
+    const url = urlEdit.value;
+    urlEdit.value = url.replace(/^https:\/\/x.com/, "https://twitter.com");
+    // alert(reurl);
+
+    // copy to clipboard
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(urlEdit.value);
+    }
+    else {
+      oldClip(urlEdit.value);
+    }
+
+    alert(`${urlEdit.value}\nをクリップボードにコピーしました！`);
   }
-});
+
+  // 古いタイプのクリップボード操作
+  function oldClip(str) {
+    urlEdit.select();
+    document.execCommand('copy');
+
+    // 選択解除
+    window.getSelection().removeAllRanges();    
+  }
+})();
